4f9ee1488343563e4fb07de1b17cbfb35d0172d4
dede <dede@dede-mini>
2013-11-26 03:26:52 +0100

    Classe feu améliorée

---
+++ pc/src/Elements_de_jeu.java
@@ -0,0 +1,20 @@
+public class Elements_de_jeu {
+	public Elements_de_jeu() {}
+	protected Vector position ;
+	private static int nbStoredFires ;
+	private static Feu[] Fires ;
+	
+	
+	public int get_nbStoredFires()
+	{
+		return nbStoredFires;
+	}
+	
+	public void set_nbStoredFires(int a)
+	{
+		nbStoredFires = a ;
+	}
+	
+	
+	
+}
