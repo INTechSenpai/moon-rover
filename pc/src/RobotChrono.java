da8947ad94b62d556f9ded805d7d464e9ca9fe07
PF <troll@yopmail.fr>
2013-11-26 16:44:18 +0100

    Création des classes Robot, RobotVrai et RobotChrono

---
+++ pc/src/RobotChrono.java
@@ -0,0 +1,37 @@
+
+public class RobotChrono extends Robot {
+
+	public void stopper()
+	{
+		
+	}
+	public void avancer()
+	{
+		
+	}
+	public void correction_angle()
+	{
+		
+	}
+	public void tourner()
+	{
+		
+	}
+	public void suit_chemin()
+	{
+		
+	}
+	public void va_au_point()
+	{
+		
+	}
+	public void set_vitesse_translation()
+	{
+		
+	}
+	public void set_vitesse_rotation()
+	{
+		
+	}
+	
+}
---
+++ pc/src/RobotVrai.java
@@ -0,0 +1,38 @@
+
+public class RobotVrai extends Robot {
+
+	public void stopper()
+	{
+		
+	}
+	public void avancer()
+	{
+		
+	}
+	public void correction_angle()
+	{
+		
+	}
+	public void tourner()
+	{
+		
+	}
+	public void suit_chemin()
+	{
+		
+	}
+	public void va_au_point()
+	{
+		
+	}
+	public void set_vitesse_translation()
+	{
+		
+	}
+	public void set_vitesse_rotation()
+	{
+		
+	}
+
+	
+}
