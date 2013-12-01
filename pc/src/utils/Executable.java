c46e5882ed73f43c1c6da614373ca48c6ea843f3
PF <troll@yopmail.fr>
2013-12-01 15:18:00 +0100

    Commentaires pour Exécutable

--- pc/src/utils/Executable.java
+++ pc/src/utils/Executable.java
@@ -1,9 +1,13 @@
 package utils;
 
 /**
- * Interface utilisée pour passer des méthodes à Callback
+ * Interface utilisée pour passer des méthodes à Callback.
+ * Il faut créer une classe implémentant cette interface par méthode d'intérêt.
+ * Il y a alors, dans l'implémentation, des attributs pour les arguments qui sont initialisés par le constructeur,
+ * de manière à ce que execute reste sans argument.
  * @author pf
  */
+
 public interface Executable {
 
 	/**
