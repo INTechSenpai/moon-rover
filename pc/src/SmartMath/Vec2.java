ef59291e9622ddaf114f2b06ca34b370072ad271
Martial Costantini <martial.costantini@telecom-sudparis.eu>
2013-12-02 22:40:00 +0100

    Sopoc

--- pc/src/SmartMath/Vec2.java
+++ pc/src/SmartMath/Vec2.java
@@ -29,20 +29,37 @@ public class Vec2
 		y = requestedY;
 	}
 	
+	// Do not square a length, use squared length directly
+	// to increase performances
 	public float SquaredLength()
 	{
 		return x*x + y*y;
 	}
 
+	// Returns this vec2's magnitude
 	public float Length()
 	{
 		return (float) Math.sqrt(SquaredLength());
 	}
 
+	// dot product
 	public float dot(Vec2 other)
 	{
 		return x*other.x + y*other.y;
 	}
-
+	
+	// build a new Vec2 by summing the calling Vec2 and the one in args
+	public Vec2 Plus(Vec2 other)
+	{
+		return new Vec2(x + other.x, y + other.y);
+	}
+	
+	// build a new Vec2 with the value obtained by decrementing the
+	// calling Vec2 by the provided Vec2 in args
+	public Vec2 Minus(Vec2 other)
+	{
+		return new Vec2(x - other.x, y - other.y);
+	}
+	
 }
 
