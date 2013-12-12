e4dbbdc9e3c5ece3e9208339e5e9ea1ab6901a4c
PF <troll@yopmail.fr>
2013-12-12 11:36:35 +0100

    Correction memorymanager

--- pc/src/strategie/MemoryManager.java
+++ pc/src/strategie/MemoryManager.java
@@ -22,20 +22,19 @@ public class MemoryManager implements Service {
 	
 	private final int nbmax;
 
-	protected Hashtable<String, FactoryProduct> productsModels;
-	protected Hashtable<String, Integer> productsIndices;
-	protected Hashtable<String, FactoryProduct[]> productsObjects;
+	protected Hashtable<String, FactoryProduct> productsModels = new Hashtable<String, FactoryProduct>();
+	protected Hashtable<String, Integer> productsIndices = new Hashtable<String, Integer>();
+	protected Hashtable<String, FactoryProduct[]> productsObjects = new Hashtable<String, FactoryProduct[]>();
 	
 
 	public MemoryManager(Service config, Service log)
 	{
 		this.log = (Log) log;
 		this.config = (Read_Ini) config;
-
 		nbmax = Integer.parseInt(this.config.config.getProperty("nb_max_noeuds"));
-
-		register("Table");
-		register("RobotChrono");
+		
+//		register("Table");
+//		register("RobotChrono");
 	}
 
 	public void register(String nom)
@@ -44,6 +43,8 @@ public class MemoryManager implements Service {
 		{
 			log.debug("Instanciation des tables", this);
 			productsObjects.put("Table", new Table[nbmax]);
+			productsIndices.put("Table", 0);
+			productsModels.put("Table", null);
 			for(int i = 0; i < nbmax; i++)
 				productsObjects.get("Table")[i] = new Table(null, null);
 		}
@@ -51,23 +52,26 @@ public class MemoryManager implements Service {
 		{
 			log.debug("Instanciation des robotchrono", this);
 			productsObjects.put("RobotChrono", new RobotChrono[nbmax]);
+			productsIndices.put("RobotChrono", 0);
+			productsModels.put("RobotChrono", null);
 			for(int i = 0; i < nbmax; i++)
 				productsObjects.get("RobotChrono")[i] = new RobotChrono(null, null);
 		}
 		else
 			log.warning("Erreur lors de l'enregistrement de "+nom, this);
+
 	}
 	
 	public void setModele(FactoryProduct instance)
 	{
-		productsModels.put(instance.TypeName(), instance);
-		productsIndices.put(instance.TypeName(), 0);		
+		productsModels.put(instance.TypeName(), instance.Clone());
 	}
 	
 	public FactoryProduct getClone(String nom)
 	{
 		FactoryProduct out = productsObjects.get(nom)[productsIndices.get(nom)];
 		out = productsModels.get(nom).Clone();
+		productsIndices.put(nom, (productsIndices.get(nom)+1) % nbmax);
 		return out;
 	}
 			
