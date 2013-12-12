551ca9345d1d0e728e49666d72fbc8347a1be5e5
PF <troll@yopmail.fr>
2013-12-12 01:16:54 +0100

    Exception dans capteur

--- pc/src/robot/cartes/Capteur.java
+++ pc/src/robot/cartes/Capteur.java
@@ -42,29 +42,36 @@ public class Capteur implements Service {
 		String[] infrarouges;
 		int[] distances;
 		
-    	if(marche_arriere)
-    	{
-    		distances = new int[nb_capteurs_ultrason_arriere+nb_capteurs_infrarouge_arriere];
-    		ultrasons = serie.communiquer("us_arr", nb_capteurs_ultrason_arriere);
-    		infrarouges  = serie.communiquer("ir_arr", nb_capteurs_infrarouge_arriere);
-    		for(int i = 0; i < nb_capteurs_ultrason_arriere; i++)
-    			distances[i] = Integer.parseInt(ultrasons[i]);
-    		for(int i = 0; i < nb_capteurs_infrarouge_arriere; i++)
-    			distances[nb_capteurs_ultrason_arriere+i] = Integer.parseInt(infrarouges[i]);
-    	}
-    	else
-    	{
-    		distances = new int[nb_capteurs_ultrason_avant+nb_capteurs_infrarouge_avant];
-    		ultrasons = serie.communiquer("us_av", nb_capteurs_ultrason_avant);
-    		infrarouges  = serie.communiquer("ir_av", nb_capteurs_infrarouge_avant);
-    		for(int i = 0; i < nb_capteurs_ultrason_avant; i++)
-    			distances[i] = Integer.parseInt(ultrasons[i]);
-    		for(int i = 0; i < nb_capteurs_infrarouge_avant; i++)
-    			distances[nb_capteurs_ultrason_avant+i] = Integer.parseInt(infrarouges[i]);
-    	}
-    	
-    	Arrays.sort(distances); // le dernier élément d'un tableau trié par ordre croissant est le plus grand
-    	return distances[distances.length-1];
+		try{
+	    	if(marche_arriere)
+	    	{
+	    		distances = new int[nb_capteurs_ultrason_arriere+nb_capteurs_infrarouge_arriere];
+	    		ultrasons = serie.communiquer("us_arr", nb_capteurs_ultrason_arriere);
+	    		infrarouges  = serie.communiquer("ir_arr", nb_capteurs_infrarouge_arriere);
+	    		for(int i = 0; i < nb_capteurs_ultrason_arriere; i++)
+	    			distances[i] = Integer.parseInt(ultrasons[i]);
+	    		for(int i = 0; i < nb_capteurs_infrarouge_arriere; i++)
+	    			distances[nb_capteurs_ultrason_arriere+i] = Integer.parseInt(infrarouges[i]);
+	    	}
+	    	else
+	    	{
+	    		distances = new int[nb_capteurs_ultrason_avant+nb_capteurs_infrarouge_avant];
+	    		ultrasons = serie.communiquer("us_av", nb_capteurs_ultrason_avant);
+	    		infrarouges  = serie.communiquer("ir_av", nb_capteurs_infrarouge_avant);
+	    		for(int i = 0; i < nb_capteurs_ultrason_avant; i++)
+	    			distances[i] = Integer.parseInt(ultrasons[i]);
+	    		for(int i = 0; i < nb_capteurs_infrarouge_avant; i++)
+	    			distances[nb_capteurs_ultrason_avant+i] = Integer.parseInt(infrarouges[i]);
+	    	}
+	    	
+	    	Arrays.sort(distances); // le dernier élément d'un tableau trié par ordre croissant est le plus grand
+	    	return distances[distances.length-1];
+		}
+		catch(Exception e)
+		{
+			log.critical(e.toString(), this);
+			return 3000; // valeur considérée comme infinie
+		}
     }
 	
     public boolean demarrage_match()
