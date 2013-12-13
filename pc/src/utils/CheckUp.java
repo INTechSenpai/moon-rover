b673c61a796fd2e14b79c22937ec971da0fa621b
PF <troll@yopmail.fr>
2013-12-13 11:59:46 +0100

    Classe check-up

---
+++ pc/src/utils/CheckUp.java
@@ -0,0 +1,27 @@
+package utils;
+
+import robot.RobotVrai;
+
+/**
+ * Service qui permettra de faire un checkup du robot avant le match
+ * @author pf
+ *
+ */
+public class CheckUp {
+
+	private Log log;
+	private RobotVrai robotvrai;
+	
+	public CheckUp(Log log, RobotVrai robotvrai)
+	{
+		this.robotvrai = robotvrai;
+		this.log = log;
+	}
+	
+	// TODO check-up du robot
+	public void lancer()
+	{
+		
+	}
+	
+}
