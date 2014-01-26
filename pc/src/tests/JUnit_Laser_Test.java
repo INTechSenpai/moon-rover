96763e58b93305694618a511e973880407b7bcab
PF <troll@yopmail.fr>
2014-01-26 18:02:03 +0100

    tests laser

---
+++ pc/src/tests/JUnit_Laser_Test.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import robot.cartes.Laser;
+
+public class JUnit_Laser_Test extends JUnit_Test {
+
+	Laser laser;
+	
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		log.debug("JUnit_Laser_Test.setUp()", this);
+		laser = (Laser) container.getService("Laser");
+		
+	}
+	
+	@Test
+	public void test_avant_verification() throws Exception
+	{
+		log.debug("JUnit_Laser_Test.test_avant_verification()", this);
+		Assert.assertTrue(laser.balises_actives().size() == 0);
+		Assert.assertTrue(laser.balises_ignorees().size() == 2);		
+	}
+
+	@Test
+	public void test_apres_verification() throws Exception
+	{
+		log.debug("JUnit_Laser_Test.test_apres_verification()", this);
+		laser.verifier_balises_connectes();
+		Assert.assertTrue(laser.balises_actives().size() == 2);
+		Assert.assertTrue(laser.balises_ignorees().size() == 0);		
+	}
+
+	@Test
+	public void test_coherence() throws Exception
+	{
+		log.debug("JUnit_Laser_Test.test_coherence()", this);
+		laser.verifier_balises_connectes();
+		laser.verifier_coherence_balise();
+	}
+
+}
