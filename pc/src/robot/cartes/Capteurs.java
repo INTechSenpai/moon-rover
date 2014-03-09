05b964b9a8566abd0f6f666d72b54c6681152697
PF <troll@yopmail.fr>
2014-03-09 13:03:37 +0100

    On peut désactiver les capteurs pour les tests

--- pc/src/robot/cartes/Capteurs.java
+++ pc/src/robot/cartes/Capteurs.java
@@ -6,6 +6,7 @@ import robot.serial.Serial;
 import utils.Log;
 import utils.Read_Ini;
 import container.Service;
+import exception.ConfigException;
 import exception.SerialException;
 
 /**
@@ -16,8 +17,11 @@ import exception.SerialException;
 public class Capteurs implements Service {
 
 	// Dépendances
+	private Read_Ini config;
 	private Log log;
 	private Serial serie;
+	
+	private boolean capteurs_on;
 
 	private final int nb_capteurs_infrarouge_avant = 1;
 //    private final int nb_capteurs_infrarouge_arriere = 0;
@@ -26,8 +30,15 @@ public class Capteurs implements Service {
     
 	public Capteurs(Read_Ini config, Log log, Serial serie)
 	{
+		this.config = config;
 		this.log = log;
 		this.serie = serie;
+		try {
+			capteurs_on = Boolean.parseBoolean(config.get("capteurs_on"));
+		} catch (ConfigException e) {
+			capteurs_on = true;
+			e.printStackTrace();
+		}
 	}
     
 	/**
@@ -37,34 +48,24 @@ public class Capteurs implements Service {
 	 */
     public int mesurer(boolean marche_arriere)
     {
+    	if(!capteurs_on)
+    		return 3000;
 		String[] ultrasons;
 		String[] infrarouges;
 		int[] distances;
 		
 		try{
-/*	    	if(marche_arriere)
-	    	{
-	    		distances = new int[nb_capteurs_ultrason_arriere+nb_capteurs_infrarouge_arriere];
-	    		ultrasons = serie.communiquer("us_arr", nb_capteurs_ultrason_arriere);
-	    		infrarouges  = serie.communiquer("ir_arr", nb_capteurs_infrarouge_arriere);
-	    		for(int i = 0; i < nb_capteurs_ultrason_arriere; i++)
-	    			distances[i] = Integer.parseInt(ultrasons[i]);
-	    		for(int i = 0; i < nb_capteurs_infrarouge_arriere; i++)
-	    			distances[nb_capteurs_ultrason_arriere+i] = Integer.parseInt(infrarouges[i]);
-	    	}
-	    	else
-	    	{*/
-	    		distances = new int[nb_capteurs_ultrason_avant+nb_capteurs_infrarouge_avant];
-	    		ultrasons = serie.communiquer("us_av", nb_capteurs_ultrason_avant);
-	    		infrarouges  = serie.communiquer("ir_av", nb_capteurs_infrarouge_avant);
-	    		for(int i = 0; i < nb_capteurs_ultrason_avant; i++)
-	    			distances[i] = Integer.parseInt(ultrasons[i]);
-	    		for(int i = 0; i < nb_capteurs_infrarouge_avant; i++)
-	    			distances[nb_capteurs_ultrason_avant+i] = Integer.parseInt(infrarouges[i]);
-//	    	}
+    		distances = new int[nb_capteurs_ultrason_avant+nb_capteurs_infrarouge_avant];
+    		ultrasons = serie.communiquer("us_av", nb_capteurs_ultrason_avant);
+    		infrarouges  = serie.communiquer("ir_av", nb_capteurs_infrarouge_avant);
+    		for(int i = 0; i < nb_capteurs_ultrason_avant; i++)
+    			distances[i] = Integer.parseInt(ultrasons[i]);
+    		for(int i = 0; i < nb_capteurs_infrarouge_avant; i++)
+    			distances[nb_capteurs_ultrason_avant+i] = Integer.parseInt(infrarouges[i]);
 	    	
 	    	Arrays.sort(distances); // le dernier élément d'un tableau trié par ordre croissant est le plus grand
 	    	int distance = distances[distances.length-1];
+	    	
 	    	if(distance < 0)
 	    		return 3000;
 	    	return distance;
