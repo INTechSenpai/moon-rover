54864c2ccbea94abbd7799cf2d5cdcbfd5b1a9d8
kayou <pierre.bourre@telecom-sudparis.eu>
2014-03-11 20:29:34 +0100

    Pour que l'upload et le code fonctionne sur la raspberry

--- pc/upload/upload.sh
+++ pc/upload/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LOGIN=intech
-DOSSIER_DESTINATION=/home/intech/intech-2013/software/pc/
+DOSSIER_DESTINATION=/home/intech/intech-2014/software/pc/
 
 rsync -e ssh --delete-after --exclude-from exclusion.txt -az ../ "$LOGIN"@"$1":"$DOSSIER_DESTINATION"
