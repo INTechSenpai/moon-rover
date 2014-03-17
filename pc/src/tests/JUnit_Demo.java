e56ca290e425915d38d13ab6493377223ab41d02
Dede <dede@plopmail.fr>
2014-03-17 19:09:03 +0100

    Démo pour le 18/3

--- pc/src/tests/JUnit_Demo.java
+++ pc/src/tests/JUnit_Demo.java
@@ -1,11 +1,20 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import hook.Callback;
+import hook.Executable;
+import hook.Hook;
 import hook.HookGenerator;
+import hook.methodes.TakeFire;
+import hook.methodes.TirerBalles;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import robot.Cote;
 import robot.RobotChrono;
 import robot.RobotVrai;
 import scripts.Script;
@@ -38,7 +47,6 @@ public class JUnit_Demo extends JUnit_Test {
 		container.getService("threadPosition");
 		container.demarreThreads();
 		robotvrai.set_vitesse_translation("30");
-		robotvrai.setPosition(new Vec2(1300, 1200));
 	}
 
 	@Test
@@ -50,11 +58,77 @@ public class JUnit_Demo extends JUnit_Test {
 	
 	@Test
 	public void arbre() throws Exception {
-		robotvrai.avancer(500);
-/*		robotvrai.tourner(-((float)(Math.PI /2)));
-		robotvrai.avancer(700);
-*/		s = (Script)scriptmanager.getScript("ScriptTree");
+		s = (Script)scriptmanager.getScript("ScriptTree");
 		s.agit(1, robotvrai, table, true);
+		robotvrai.avancer(500);
 	}
 
+	@Test
+	public void ramasse_feu_droit() throws Exception
+	{
+		robotvrai.avancer(100);
+		robotvrai.takefire(Cote.DROIT);
+	}
+	
+	@Test
+	public void ramasse_feu_gauche() throws Exception
+	{
+		robotvrai.avancer(100);
+		robotvrai.takefire(Cote.GAUCHE);
+	}
+	
+	@Test
+	public void depose_fruits() throws Exception
+	{
+		s = (Script)scriptmanager.getScript("ScriptDeposerFruits");
+		s.agit(0, robotvrai, table, false);
+	}
+	
+	@Test
+	public void tir_balles() throws Exception
+	{
+		s = (Script)scriptmanager.getScript("ScriptLances");
+		s.agit(0, robotvrai, table, true);
+	}
+	
+	@Test
+	public void depose_feu_gauche() throws Exception
+	{
+		robotvrai.avancer(100);
+		robotvrai.lever_pince(Cote.DROIT);
+		robotvrai.lever_pince(Cote.GAUCHE);
+		robotvrai.takefire(Cote.GAUCHE);
+		s = (Script)scriptmanager.getScript("ScriptDeposerFeu");
+		s.agit(2, robotvrai, table, true);
+	}
+
+	@Test
+	public void depose_feu_droit() throws Exception
+	{
+		robotvrai.avancer(100);
+		robotvrai.lever_pince(Cote.DROIT);
+		robotvrai.lever_pince(Cote.GAUCHE);
+		robotvrai.takefire(Cote.DROIT);
+		s = (Script)scriptmanager.getScript("ScriptDeposerFeu");
+		s.agit(2, robotvrai, table, true);
+	}
+	
+	@Test
+	public void choré1() throws Exception
+	{
+		hookgenerator = (HookGenerator)container.getService("HookGenerator");
+		ArrayList<Hook> hooks = new ArrayList<Hook>();
+		Executable takefire = new TakeFire(robotvrai, Cote.DROIT);
+		Hook hook = hookgenerator.hook_feu(Cote.GAUCHE);
+		hook.ajouter_callback(new Callback(takefire, false));
+		hooks.add(hook);
+		robotvrai.avancer(1000,hooks);
+		robotvrai.tourner((float)(3*(Math.PI)/2));
+		robotvrai.avancer(500,hooks);
+		robotvrai.tourner(0);
+		robotvrai.avancer(1000,hooks);
+	}
+	
+	
 }
+
