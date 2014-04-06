65580dd908209ffb97d5da9e73fb22a44b926e37
PF <troll@yopmail.fr>
2014-04-07 01:14:03 +0200

    conseils merge

--- pc/src/pathfinding/cache/CacheLoader.java
+++ pc/src/pathfinding/cache/CacheLoader.java
@@ -9,6 +9,15 @@ import java.io.*;
  *
  */
 
+/*
+ * Deux possibilités pour merger: 
+ * - soit tu mets ce code comme méthode static dans CacheHolder, et tu feras cache = CacheHolder.loadCacheFile("abwabwa");
+ * - soit tu mets ce code dans le constructeur de CacheHolder, et tu feras cache = new CacheHolder("abwabwa");
+ * Je préfère le 2e cas, les méthodes statiques c'est mal.
+ * Souviens-toi juste que dans le deuxième cas, tu dois définir explicitement le constructeur par défaut (sans argument), même s'il est vide.
+ * PF
+ */
+
 public class CacheLoader 
 {
 	
