d30d17124d265b601494faa06bccb2a822bff32e
Martial <M4rsy4ll@gmail.com>
2014-04-15 23:59:51 +0200

    Avancement et proprisation de l'arbre. La facon de le parcourir fait toujour débat en moi, je vais bientot le traiter.

--- pc/src/strategie/arbre/Branche.java
+++ pc/src/strategie/arbre/Branche.java
@@ -3,6 +3,9 @@
  */
 package strategie.arbre;
 
+import java.util.ArrayList;
+
+import scripts.Script;
 import table.Table;
 
 /**
@@ -20,29 +23,109 @@ import table.Table;
  */
 public class Branche 
 {
+	
+	
 	private long date;			// date d'évaluation de cette branche
 	private int duree_totale;	// temps d'exécution de la partie de l'arbre en amont de cette branche
 	private int profondeur;		// profondeur a laquelle cettre branche sera explorée (taille de l'arbre en aval de cette branche)
 	private int id_robot;		// robot qui parcours la branche (?)
 	
+	// note attribuée a cette branche
+	public float note;
 	
+	// Action a executer entre le sommet juste avant cette branche et l'état final
+	public Script script;
+	public int metaversion;
+	
+	// Table résultant de la première action de cette branche
 	public Table etatFinal;
+	
+	// Sous branches contenant toutes les autres actions possibles a partir de l'état final
+	public ArrayList<Branche> sousBranches;
+	
+	
+	
 	
 	/** Constructeur de la branche
 	 * @param date
 	 * @param duree_totale
 	 * @param profondeur
 	 * @param id_robot
+	 * @param script
+	 * @param metaversion
 	 */
-	public Branche(long date, int duree_totale, int profondeur, int id_robot) 
+	public Branche(long date, int duree_totale, int profondeur, int id_robot, Script script, int metaversion )
 	{
+		
 		this.date = date;
 		this.duree_totale = duree_totale;
 		this.profondeur = profondeur;
 		this.id_robot = id_robot;
+		this.script = script;
+		this.metaversion = metaversion;
 	}
 	
+	/*
 	
+	Itterative DFS:
+	
+	Put origin in S
+	
+	while S.size
+		v = S.first
+		S.pop
+		put all childs of v at the beginning of S
+		
+		
+	REcursive DFS :
+	
+	For all origin chlidren :
+		DFS(children[i])
+	
+	
+	*/
+	
+	/* Méthode qui calcule la note de cette branche en calculant celles de ses sous branches, puis en combinant leur notes
+	 * C'est là qu'est logé le DFS
+	 * 
+	 */
+	public void evaluate()
+	{
+		// Si la profondeur est non nulle (ie s'il y a des sous-branches), on explore toutes les sous-branches
+		if (profondeur > 0)
+		{
+			// Soucis : l'algo qu'on utilise est pas stricto sensu un DFS, car un traitement est effectué au débouclage (le calcul de la note)
+			// a voir si utiliser un itterative DFS est plus performant ou non.
+			
+		}
+		
+		// Finalement, donne une note a toute cette branche
+		computeNote();
+	}
+	
+	/** Méthode qui prend les notes de chaque sous branche (en supposant qu'elles sont déjà calculés et qu'il n'y a plus qu'a
+	 * les considérer) et les mélange popur produire la note de cette branche
+	 * Il n'y a que cette méthode qui doit modifier this.note
+	 */
+	public void computeNote()
+	{
+		if(profondeur == 0)
+		{
+			// note = script.note
+		}
+		else
+		{
+			// TODO : mixer les notes des sous-branches
+			
+			// pour l'instant, un simple max ira bien :
+			note = -1;	// Valeur incorecte pour forcer la réattribution
+			for (int i = 0; i< sousBranches.size(); ++i)
+				if(sousBranches.get(i).note > this.note)
+					note = sousBranches.get(i).note; 
+		}
+		
+	}
+
 	
 	
 	
