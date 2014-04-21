a76829fabd00acdb52f9f535feb5faf01d22b708
PF <troll@yopmail.fr>
2014-04-21 13:54:31 +0200

    Réécriture SearchConst

--- pc/src/asservissement/SearchConst.java
+++ pc/src/asservissement/SearchConst.java
@@ -3,155 +3,92 @@ package asservissement;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import container.Container;
-import exception.ConfigException;
-import exception.ContainerException;
-import exception.SerialException;
-import exception.SerialManagerException;
-import exception.ThreadException;
 import robot.cartes.Deplacements;
 
+/**
+ * Application permettant de trouver les constantes d'asservissement en rotation
+ * et en translation
+ * 
+ * @author pf, kayou
+ * 
+ */
 public class SearchConst
 {
+    private static float kp = 0, kd = 0;
+    private static int pwm_max = 0;
+    private static BufferedReader bufferRead = new BufferedReader(
+            new InputStreamReader(System.in));
 
-	public static void main(String[] args)
-	{
-		float kp = 0, kd = 0;
-		int pwm_max = 0;
-		Container container;
-		Deplacements deplacements = null;
-		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-		int signe = 1;
-		try
-		{
-			container = new Container();
-			deplacements = (Deplacements) container.getService("Deplacements");
-		}
-		catch (ContainerException | ThreadException | ConfigException
-				| SerialManagerException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public static void main(String[] args)
+    {
+        Container container;
+        Deplacements deplacements = null;
+        int signe = 1;
+        try
+        {
+            container = new Container();
+            deplacements = (Deplacements) container.getService("Deplacements");
 
-		System.out.println("r ou t?");
-		char asserv = 'a';
-		try
-		{
-			asserv = (char) System.in.read();
-		}
-		catch (IOException e2)
-		{
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
+            System.out.println("r ou t?");
+            char asserv = (char) System.in.read();
+            float distance = -1000;
+            float angle = -(float) Math.PI;
 
-		float distance = -1000;
-		float angle = - (float) Math.PI;
+            while (true)
+            {
+                if (asserv == 't')
+                {
+                    distance = -distance;
 
-		while (true)
-		{
-			if(asserv == 't')
-			{
-				distance = - distance;
-				try
-				{
-					String s;
-					if (bufferRead.ready())
-					{
-						bufferRead.readLine();
-					}
-					System.out.println("kp ?");
-					s = bufferRead.readLine();
-					kp = Float.parseFloat(s);
-					System.out.println(kp);
-					System.out.println("kd ?");
-					s = bufferRead.readLine();
-					kd = Float.parseFloat(s);
-					System.out.println(kd);
-					System.out.println("pwm_max ?");
-					s = bufferRead.readLine();
-					pwm_max = Integer.parseInt(s);
-					System.out.println(pwm_max);
+                    if (bufferRead.ready())
+                        bufferRead.readLine();
 
-				}
-				catch (IOException e1)
-				{
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				try
-				{
-					deplacements.change_const_translation(kp, kd, pwm_max);
-				}
-				catch (SerialException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                    set_kp_kd_pwm();
 
-				try
-				{
-					System.out.println(distance);
-					deplacements.avancer(distance);
-				}
-				catch (SerialException e2)
-				{
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}
-			}
-			else if(asserv == 'r')
-			{
-				signe *= -1;
-				angle = (float) (Math.PI/2 + signe*Math.PI/2);
-				try
-				{
-					String s;
-					if (bufferRead.ready())
-					{
-						bufferRead.readLine();
-					}
-					System.out.println("kp ?");
-					s = bufferRead.readLine();
-					kp = Float.parseFloat(s);
-					System.out.println(kp);
-					System.out.println("kd ?");
-					s = bufferRead.readLine();
-					kd = Float.parseFloat(s);
-					System.out.println(kd);
-					System.out.println("pwm_max ?");
-					s = bufferRead.readLine();
-					pwm_max = Integer.parseInt(s);
-					System.out.println(pwm_max);
+                    deplacements.change_const_translation(kp, kd, pwm_max);
+                    System.out.println(distance);
+                    deplacements.avancer(distance);
 
-				}
-				catch (IOException e1)
-				{
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				try
-				{
-					deplacements.change_const_rotation(kp, kd, pwm_max);
-				}
-				catch (SerialException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				try
-				{
-					System.out.println(angle);
-					deplacements.tourner(angle);
-				}
-				catch (SerialException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                }
+                else if (asserv == 'r')
+                {
+                    signe *= -1;
+                    angle = (float) (Math.PI / 2 + signe * Math.PI / 2);
+                    if (bufferRead.ready())
+                        bufferRead.readLine();
 
-			}
-		}
-	}
+                    set_kp_kd_pwm();
+
+                    deplacements.change_const_rotation(kp, kd, pwm_max);
+                    System.out.println(angle);
+                    deplacements.tourner(angle);
+
+                }
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void set_kp_kd_pwm() throws IOException
+    {
+        String s;
+        System.out.println("kp ?");
+        s = bufferRead.readLine();
+        kp = Float.parseFloat(s);
+        System.out.println(kp);
+
+        System.out.println("kd ?");
+        s = bufferRead.readLine();
+        kd = Float.parseFloat(s);
+        System.out.println(kd);
+
+        System.out.println("pwm_max ?");
+        s = bufferRead.readLine();
+        pwm_max = Integer.parseInt(s);
+        System.out.println(pwm_max);
+    }
 }
