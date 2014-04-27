2b4fac17e9681e7c1354125e284a42bdd4f9b521
PF <troll@yopmail.fr>
2014-04-27 22:37:22 +0200

    Debug script

--- pc/src/scripts/ScriptManager.java
+++ pc/src/scripts/ScriptManager.java
@@ -46,7 +46,7 @@ public class ScriptManager implements Service {
 		scripts_robot.add("ScriptDeposerFruits");
 		scripts_robot.add("ScriptFunnyAction");
 		scripts_robot.add("ScriptDeposerFeu");
-		scripts_robot.add("ScriptFeuDebout");
+//		scripts_robot.add("ScriptFeuDebout");
 		scripts_robot.add("ScriptFeuBord");
 	}
 	
