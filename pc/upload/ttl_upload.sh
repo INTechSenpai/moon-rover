bbcae6d6e6fd0d124b0156183bc825701cab99bf
PF <techthetroll@gmail.com>
2015-06-18 21:30:27 +0200

    Correction de l'upload ttl

--- pc/upload/ttl_upload.sh
+++ pc/upload/ttl_upload.sh
@@ -4,11 +4,12 @@ if [ "$#" -ne 2 ]; then
 else
     echo "Transfert..."
     echo "stty -echo" > $2
+    sleep 0.1
     echo -n "echo \"" >> $2
     base64 --wrap=0 $1 >> $2
     echo "\"| base64 -d > $(date +%F_%T)_$1" >> $2
-    echo -n "if [ $(crc32 $1) != \"$" >> $2
-    echo "(crc32 $(date +%F_%T)_$1)\" ]; then echo "Erreur de checksum"; fi" >> $2
+    echo -n "if [ $(sha1sum $1 | cut -d " " -f 1) != \"$" >> $2
+    echo "(sha1sum $(date +%F_%T)_$1 | cut -d \" \" -f 1)\" ]; then echo "Erreur de checksum"; fi" >> $2
     echo "stty echo" >> $2
     echo "Done."
 fi
