979f29b614fc8f64263fa0b888cbad41790af238
PF <techthetroll@gmail.com>
2015-08-22 00:37:59 +0200

    Diminution du nombre de hooks à envoyer à la STM

--- pc/src/scripts/ScriptHookNames.java
+++ pc/src/scripts/ScriptHookNames.java
@@ -7,15 +7,24 @@ package scripts;
  */
 
 public enum ScriptHookNames {
-	EXEMPLE(false),
-	FUNNY_ACTION(false);
+	EXEMPLE(false, 1),
+	FUNNY_ACTION(false, 0);
 	
 	private boolean canIDoIt; // ce booléan dépend du robot!
 	// si on a deux robots, ils ne pourront pas faire la même chose...
 	
-	ScriptHookNames(boolean canIDoIt)
+	// Le numéro du capteur qui déclenche ce script
+	private int nbCapteur;
+	
+	ScriptHookNames(boolean canIDoIt, int nbCapteur)
 	{
 		this.canIDoIt = canIDoIt;
+		this.nbCapteur = nbCapteur;
+	}
+	
+	public int getNbCapteur()
+	{
+		return nbCapteur;
 	}
 	
 	public boolean canIDoIt()
