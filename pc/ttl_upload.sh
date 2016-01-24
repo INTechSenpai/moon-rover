3c809c4746525032c22c50d84eb542c6c5c40c07
PF <techthetroll@gmail.com>
2016-01-24 12:41:03 +0100

    Suppression du dernier code de Deboc qui restait sur le robot : l'upload vers la raspberry. Qui date de l'époque où on codait en python…

--- pc/ttl_upload.sh
+++ pc/ttl_upload.sh
@@ -7,9 +7,9 @@ else
     sleep 0.1
     echo -n "echo \"" >> $2
     base64 --wrap=0 $1 >> $2
-    echo "\"| base64 -d > $(date +%F_%T)_$1" >> $2
+    echo "\"| base64 -d > $(date +%F_%s)_$1" >> $2
     echo -n "if [ $(sha1sum $1 | cut -d " " -f 1) != \"$" >> $2
-    echo "(sha1sum $(date +%F_%T)_$1 | cut -d \" \" -f 1)\" ]; then echo "Erreur de checksum"; fi" >> $2
+    echo "(sha1sum $(date +%F_%s)_$1 | cut -d \" \" -f 1)\" ]; then echo "Erreur de checksum"; fi" >> $2
     echo "stty echo" >> $2
     echo "Done."
 fi
