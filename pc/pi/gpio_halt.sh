587e822c9697b2e5fac2138c082a66077aee6412
PF <techthetroll@gmail.com>
2016-03-27 20:52:35 +0200

    Conversion IR

--- pc/pi/gpio_halt.sh
+++ pc/pi/gpio_halt.sh
@@ -2,11 +2,12 @@
 if [ "$#" -ne 2 ]; then
     echo "Usage: ./gpio_halt.sh pinDiode pinHalt"
 else
+    gpio mode $1 out # diode en output
+    gpio mode $2 down # interrupteur pull-down
     gpio write $1 1 # allumage de la diode témoin
 
     # on s'arrête si la pin passe à l'état haut
-    # comme ça, si le bouton se décroche à cause d'une collision avec RCVA
-    # la raspbe ne s'éteint pas
+    # en cas de problème, vu qu'on est en pull-down on ne s'éteint pas
     while [ $(gpio read $2) -eq 0 ]
     do
         sleep 2
