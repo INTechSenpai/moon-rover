f556300ec568940077661e1bd062a09f45e68c48
PF <pierre-francois.gimenez@irit.fr>
2016-08-22 10:12:46 +0200

    Suppression du bas niveau de T3

--- arm/src/serie.cpp
+++
@@ -1,6 +0,0 @@
-#include "serie.h"
-
-uint16_t volatile idPaquetEnvoi = 0;
-SemaphoreHandle_t serial_rb_mutex_TX = xSemaphoreCreateMutex();
-unsigned char paquetsEnvoyes[TAILLE_BUFFER_ECRITURE_SERIE][40];
-uint8_t longueurPaquets[TAILLE_BUFFER_ECRITURE_SERIE];
