75b161e596d302071955c823c34958165e3cfbb2
PF <techthetroll@gmail.com>
2017-02-11 14:41:41 +0100

    Update entête de fichiers

--- pc/src/capteurs/CapteursRobot.java
+++ pc/src/capteurs/CapteursRobot.java
@@ -1,7 +1,7 @@
 package capteurs;
 
 /*
-Copyright (C) 2016 Pierre-François Gimenez
+Copyright (C) 2013-2017 Pierre-François Gimenez
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
