54fd4af99f796197a608d2672713a759f18a1ace
PF <pierre-francois.gimenez@laposte.net>
2017-02-22 19:07:54 +0100

    Config files of the rpi

--- pc/pi/configure.sh
+++ pc/pi/configure.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 sudo apt-get update -y
 
-sudo apt-get install git \
+sudo apt-get install -y git \
 oracle-java7-jdk \
 librxtx-java \
 ant \
 hostapd \
-dnsmasq
+dnsmasq \
+vim
 
 # récupération du code
 git clone https://github.com/INTechSenpai/moon-rover.git
+git init --bare moon-rover-rpi.git
+
+# création du dépôt nu local
+cd moon-rover/pc
+git remote set-url origin ~/moon-rover-rpi.git
+git push
 
 # compilation
-cd moon-rover/pc
 ant
 
 # installation de la lib série (TODO)
@@ -28,15 +34,10 @@ sudo ifdown wlan0
 sudo ifup wlan0
 
 sudo cp pi/hostapd.conf /etc/hostapd/hostapd.conf
-sudo /usr/sbin/hostapd /etc/hostapd/hostapd.conf
 sudo cp pi/hostapd /etc/default/hostapd
 sudo cp pi/dnsmasq.conf /etc/dnsmasq.conf
 sudo cp pi/sysctl.conf /etc/sysctl.conf
-
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
-sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
-sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
-
+sudo cp pi/iptables.ipv4.nat /etc/iptables.ipv4.nat
 sudo cp pi/rc.local /etc/rc.local
+
 sudo reboot
