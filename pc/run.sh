11472bce21f75d03697309aec3e7ab3a2d9c257c
PF <pierre-francois.gimenez@laposte.net>
2017-02-24 11:35:21 +0100

    nice et ionice

--- pc/run.sh
+++ pc/run.sh
@@ -3,6 +3,5 @@ export DISPLAY=:1
 if [ "$#" -lt 1 ]; then
     echo "Usage: ./run.sh Classe"
 else
-    #sudo nice -n -2
-    java -Xmx1G -Xms1G -cp bin/:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni $@
+    sudo nice -n -10 ionice -c 1 java -Xmx1G -Xms1G -cp bin/:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni $@
 fi
--- pc/run_junit.sh
+++ pc/run_junit.sh
@@ -3,5 +3,5 @@ export DISPLAY=:1
 if [ "$#" -lt 1 ]; then
     echo "Usage: ./run.sh Classe#methode"
 else
-    sudo nice -n -2 java -Xmx1G -Xms1G -cp bin/:lib/hamcrest-core-1.3.jar:lib/junit.jar:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni tests.SingleJUnitTestRunner $@
+    sudo nice -n -10 ionice -c 1 java -Xmx1G -Xms1G -cp bin/:lib/hamcrest-core-1.3.jar:lib/junit.jar:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni tests.SingleJUnitTestRunner $@
 fi
