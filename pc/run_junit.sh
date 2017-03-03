b2a5943d5ebf9843d2d0624404190f53740704fb
PF <pierre-francois.gimenez@laposte.net>
2017-03-03 22:00:30 +0100

    Please be nice senpai !

--- pc/run_junit.sh
+++ pc/run_junit.sh
@@ -3,6 +3,5 @@
 if [ "$#" -lt 1 ]; then
     echo "Usage: ./run.sh Classe#methode"
 else
-    #sudo nice -n -2
-    java -Xmx1G -Xms1G -cp bin/:lib/hamcrest-core-1.3.jar:lib/junit.jar:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni tests.SingleJUnitTestRunner $@
+    sudo nice -n -2 java -Xmx1G -Xms1G -cp bin/:lib/hamcrest-core-1.3.jar:lib/junit.jar:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni tests.SingleJUnitTestRunner $@
 fi
