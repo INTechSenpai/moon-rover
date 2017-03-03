5370ef2b319506bbb23327fe1edf856db9fbb52a
PF <pierre-francois.gimenez@laposte.net>
2017-03-03 21:25:46 +0100

    Run test on rpi

--- pc/run.sh
+++ pc/run.sh
@@ -4,5 +4,5 @@ if [ "$#" -lt 1 ]; then
     echo "Usage: ./run.sh Classe"
 else
     #sudo nice -n -2
-    java -Xmx1G -Xms1G -cp bin/:lib/jcommon-1.0.13.jar:lib/jfreechart-1.0.13.jar:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni $@
+    java -Xmx1G -Xms1G -cp bin/:lib/RXTXcomm.jar -Djava.library.path=/usr/lib/jni $@
 fi
