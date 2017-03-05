9e0f46a244f7cf1097f0ff53c82b5bbc3d9b1b4a
PF <pierre-francois.gimenez@laposte.net>
2017-03-05 15:41:26 +0100

    Debug

--- pc/src/serie/SerieCoucheTrame.java
+++ pc/src/serie/SerieCoucheTrame.java
@@ -170,6 +170,7 @@ public class SerieCoucheTrame implements Service, SerialClass
 					restart = true;
 			} catch (ProtocolException | IncorrectChecksumException | MissingCharacterException e) {
 				log.warning(e);
+				e.printStackTrace();
 				restart = true;
 			}
 		} while(restart);
