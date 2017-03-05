29e254a5907c7a5f934dd33f7b69f2afbb692517
PF <pierre-francois.gimenez@laposte.net>
2017-03-05 22:23:24 +0100

    test

--- pc/src/tests/lowlevel/JUnit_Robot.java
+++ pc/src/tests/lowlevel/JUnit_Robot.java
@@ -145,6 +145,19 @@ public class JUnit_Robot extends JUnit_Test {
 		astar.process(chemin);
 		robot.followTrajectory(true, Speed.TEST);
     }
+	
+	@Test
+    public void test_follow_trajectory_courbe_gauche2() throws Exception
+    {
+		Cinematique depart = new Cinematique(0, 1800, -Math.PI/2, true, 0, Speed.STANDARD.translationalSpeed);
+		robot.setCinematique(depart);
+		data.correctPosition(depart.getPosition(), depart.orientationReelle); // on envoie la position haut niveau
+		Thread.sleep(100); // on attend un peu que la position soit affectée bas niveau
+		Cinematique c = new Cinematique(700, 1200, Math.PI, false, 0, Speed.STANDARD.translationalSpeed);
+		astar.initializeNewSearch(c, true, state);
+		astar.process(chemin);
+		robot.followTrajectory(true, Speed.TEST);
+    }
 
 	@Test
     public void test_follow_trajectory_courbe_droite() throws Exception
