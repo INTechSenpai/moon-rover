fa35425b14761b24d455cbeaf4ce3557b7034ca0
PF <techthetroll@gmail.com>
2017-04-30 19:08:01 +0200

    Test ?

--- pc/src/threads/serie/ThreadSerialInputCoucheOrdre.java
+++ pc/src/threads/serie/ThreadSerialInputCoucheOrdre.java
@@ -147,7 +147,7 @@ public class ThreadSerialInputCoucheOrdre extends ThreadService implements Seria
 								double angleRoueGauche = -(data[6] - 150.)*Math.PI/180.;
 								double angleRoueDroite = -(data[7] - 150.)*Math.PI/180.;
 								
-								if(debugCapteurs)
+								if(debugSerie || debugCapteurs)
 									log.debug("Angle roues : à gauche "+data[6]+", à droite "+data[7]);
 								
 								/**
