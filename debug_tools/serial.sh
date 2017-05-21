d78f3a20b6e8fac07126697833a434106a8462af
Sylvain <sylvain.gaultier@telecom-sudparis.eu>
2017-05-22 00:31:54 +0200

    script serial maj exp2

--- debug_tools/serial.sh
+++ debug_tools/serial.sh
@@ -1,7 +1,5 @@
 # Permet de communiquer avec l'interface débug du bas niveau depuis la raspberry pi
-command=sudo python -m serial.tools.miniterm -e --eol LF /dev/ttyACM0
-
 while [ true ]; do
-	$command
+	sudo python -m serial.tools.miniterm -e --eol LF /dev/ttyACM0
 	sleep 2
 done
