1739dcce7cdddb9cceb2379318ebea5ac43aaa8a
sylvain.gaultier <sylvain.gaultier@telecom-sudparis.eu>
2017-06-04 00:17:44 +0200

    mapping boutons wiimote

---
+++ wiimote/low_level_interface.py
@@ -0,0 +1,47 @@
+def set_speed(speed):
+    print "Speed set to: " + str(speed)
+    # @PF: au travail !
+
+def set_direction(direction):
+    print "Direction set to: " + str(direction)
+    # @PF: au travail !
+
+def robot_stop():
+    print "Stop"
+    # @PF: au travail !
+
+def robot_run():
+    print "Running"
+    # @PF: au travail !
+
+def pull_up_net():
+    print "pull_up_net"
+    # @PF: au travail !
+
+def pull_down_net():
+    print "pull_down_net"
+    # @PF: au travail !
+
+def open_net():
+    print "open_net"
+    # @PF: au travail !
+
+def close_net():
+    print "close_net"
+    # @PF: au travail !
+
+def eject_left_side():
+    print "eject_left_side"
+    # @PF: au travail !
+
+def rearm_left_side():
+    print "rearm_left_side"
+    # @PF: au travail !
+
+def eject_right_side():
+    print "eject_right_side"
+    # @PF: au travail !
+
+def rearm_right_side():
+    print "rearm_right_side"
+    # @PF: au travail !
--- wiimote/wiimote_controler_python2.py
+++ wiimote/wiimote_controler_python2.py
@@ -1,81 +1,260 @@
 import cwiid
 import time
+from low_level_interface import *
 
-button_delay = 0.1
+UPDATE_PERIOD = 0.1 # seconds
 
 print 'Press 1 + 2 on your Wii Remote now ...'
 time.sleep(1)
 
-# Connect to the Wii Remote. If it times out
-# then quit.
+# Connect to the Wii Remote. If it times out then quit.
 try:
     wii = cwiid.Wiimote()
 except RuntimeError:
+    wii = None
     print "Error opening wiimote connection"
     quit()
 
-print 'Wii Remote connected...\n'
-print 'Press some buttons!\n'
-print 'Press PLUS and MINUS together to disconnect and quit.\n'
+print 'Wii Remote connected...'
+print 'Press PLUS and MINUS together to discFalseonnect and quit.'
+
+
+# List of mode : 'wiimote_std' 'wiimote_horizontal' 'nunchuk'
+control_mode = 'wiimote_std'
+absolute_speed = 0
+speed_list = [300, 600, 1000]
+
+def increase_speed():
+    global absolute_speed
+    i = 0
+    while i < len(speed_list) - 1 and absolute_speed > speed_list[i]:
+        i += 1
+    absolute_speed = speed_list[i]
+
+def decrease_speed():
+    global absolute_speed
+    if absolute_speed <= speed_list[0]:
+        absolute_speed = 0
+    else:
+        absolute_speed -= 60
+        if absolute_speed < 0:
+            absolute_speed = 0
+
+net_open = False
+left_ejector_armed = True
+right_ejector_armed = True
+
+b_up_p = False
+b_down_p = False
+b_left_p = False
+b_right_p = False
+b_one_p = False
+b_two_p = False
+b_home_p = False
+b_A_p = False
+b_B_p = False
+
+last_update_time = 0
 
 wii.rpt_mode = cwiid.RPT_BTN
-
 while True:
-
     buttons = wii.state['buttons']
 
     # If Plus and Minus buttons pressed
     # together then rumble and quit.
     if buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0:
+        robot_stop()
         print '\nClosing connection ...'
         wii.rumble = 1
         time.sleep(1)
         wii.rumble = 0
         exit(wii)
 
-    # Check if other buttons are pressed by
-    # doing a bitwise AND of the buttons number
-    # and the predefined constant for that button.
-    if buttons & cwiid.BTN_LEFT:
-        print 'Left pressed'
-        time.sleep(button_delay)
-
-    if buttons & cwiid.BTN_RIGHT:
-        print 'Right pressed'
-        time.sleep(button_delay)
-
-    if buttons & cwiid.BTN_UP:
-        print 'Up pressed'
-        time.sleep(button_delay)
-
-    if buttons & cwiid.BTN_DOWN:
-        print 'Down pressed'
-        time.sleep(button_delay)
-
-    if buttons & cwiid.BTN_1:
-        print 'Button 1 pressed'
-        time.sleep(button_delay)
-
-    if buttons & cwiid.BTN_2:
-        print 'Button 2 pressed'
-        time.sleep(button_delay)
-
-    if buttons & cwiid.BTN_A:
-        print 'Button A pressed'
-        time.sleep(button_delay)
-
-    if buttons & cwiid.BTN_B:
-        print 'Button B pressed'
-        time.sleep(button_delay)
+    if wii.state['ext_type'] == cwiid.EXT_NUNCHUK:
+        if control_mode != 'nunchuk':
+            wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_NUNCHUK
+            control_mode = 'nunchuk'
+            robot_stop()
+            print "Control mode : nunchuk"
+    else:
+        if control_mode == 'nunchuk':
+            wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+            control_mode = 'wiimote_std'
+            robot_stop()
+            print "Control mode : wiimote_std"
 
     if buttons & cwiid.BTN_HOME:
-        print 'Home Button pressed'
-        time.sleep(button_delay)
+        if not b_home_p:
+            b_home_p = True
+            if control_mode == 'wiimote_std':
+                control_mode = 'wiimote_horizontal'
+                print "Control mode : wiimote_horizontal"
+            elif control_mode == 'wiimote_horizontal':
+                control_mode = 'wiimote_std'
+                print "Control mode : wiimote_std"
+            else:
+                print "Control mode : nunchuk"
+    else:
+        b_home_p = False
 
-    if buttons & cwiid.BTN_MINUS:
-        print 'Minus Button pressed'
-        time.sleep(button_delay)
+    if control_mode == 'wiimote_horizontal':
+        if buttons & cwiid.BTN_RIGHT:
+            if not b_right_p:
+                b_right_p = True
+                pull_up_net()
+        else:
+            b_right_p = False
 
-    if buttons & cwiid.BTN_PLUS:
-        print 'Plus Button pressed'
-        time.sleep(button_delay)
+        if buttons & cwiid.BTN_LEFT:
+            if not b_left_p:
+                b_left_p = True
+                pull_down_net()
+        else:
+            b_left_p = False
+
+        if buttons & cwiid.BTN_UP:
+            if not b_up_p:
+                b_up_p = True
+                if left_ejector_armed:
+                    eject_left_side()
+                    left_ejector_armed = False
+                else:
+                    rearm_left_side()
+                    left_ejector_armed = True
+        else:
+            b_up_p = False
+
+        if buttons & cwiid.BTN_DOWN:
+            if not b_down_p:
+                b_down_p = True
+                if right_ejector_armed:
+                    eject_right_side()
+                    right_ejector_armed = False
+                else:
+                    rearm_right_side()
+                    right_ejector_armed = True
+        else:
+            b_down_p = False
+
+        if buttons & cwiid.BTN_A:
+            if not b_A_p:
+                b_A_p = True
+                if net_open:
+                    close_net()
+                    net_open = False
+                else:
+                    open_net()
+                    net_open = True
+        else:
+            b_A_p = False
+
+        if not buttons & cwiid.BTN_B:
+            b_B_p = False
+
+        if time.time() - last_update_time > UPDATE_PERIOD:
+            last_update_time = time.time()
+
+            speed = 0
+            if buttons & cwiid.BTN_2:
+                if buttons & cwiid.BTN_B and not b_B_p:
+                    b_B_p = True
+                    absolute_speed += 1
+                increase_speed()
+            else:
+                if buttons & cwiid.BTN_B and absolute_speed != 0:
+                    b_B_p = True
+                    absolute_speed = 0
+                elif not buttons & cwiid.BTN_B:
+                    decrease_speed()
+                elif buttons & cwiid.BTN_B and not b_B_p:
+                    speed = -300
+            if speed == 0:
+                set_speed(absolute_speed)
+            else:
+                set_speed(speed)
+
+
+            # Todo direction
+
+    else:
+        if buttons & cwiid.BTN_RIGHT:
+            if not b_right_p:
+                b_right_p = True
+                if right_ejector_armed:
+                    eject_right_side()
+                    right_ejector_armed = False
+                else:
+                    rearm_right_side()
+                    right_ejector_armed = True
+        else:
+            b_right_p = False
+
+        if buttons & cwiid.BTN_LEFT:
+            if not b_left_p:
+                b_left_p = True
+                if left_ejector_armed:
+                    eject_left_side()
+                    left_ejector_armed = False
+                else:
+                    rearm_left_side()
+                    left_ejector_armed = True
+        else:
+            b_left_p = False
+
+        if buttons & cwiid.BTN_UP:
+            if not b_up_p:
+                b_up_p = True
+                pull_up_net()
+        else:
+            b_up_p = False
+
+        if buttons & cwiid.BTN_DOWN:
+            if not b_down_p:
+                b_down_p = True
+                pull_down_net()
+        else:
+            b_down_p = False
+
+        if buttons & cwiid.BTN_1:
+            if not b_one_p:
+                b_one_p = True
+                if net_open:
+                    close_net()
+                    net_open = False
+                else:
+                    open_net()
+                    net_open = True
+        else:
+            b_one_p = False
+
+        if not buttons & cwiid.BTN_B:
+            b_B_p = False
+
+        if time.time() - last_update_time > UPDATE_PERIOD:
+            last_update_time = time.time()
+
+            speed = 0
+            if buttons & cwiid.BTN_A:
+                if buttons & cwiid.BTN_B and not b_B_p:
+                    b_B_p = True
+                    absolute_speed += 1
+                increase_speed()
+            else:
+                if buttons & cwiid.BTN_B and absolute_speed != 0:
+                    b_B_p = True
+                    absolute_speed = 0
+                elif not buttons & cwiid.BTN_B:
+                    decrease_speed()
+                elif buttons & cwiid.BTN_B and not b_B_p:
+                    speed = -300
+            if speed == 0:
+                set_speed(absolute_speed)
+            else:
+                set_speed(speed)
+
+            #direction = 0
+            if control_mode == 'nunchuk':
+                pass
+            else:
+                pass
+            #set_direction(direction)
